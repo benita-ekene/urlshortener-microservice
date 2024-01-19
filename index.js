172510080bb848b7deb69085f65b47ee9a83ef27
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-19 22:24:47 +0100

    updated code

--- index.js
+++ index.js
@@ -44,6 +44,31 @@ function shortenUrl(originalUrl, callback) {
   });
 }
 
+// // Endpoint to shorten URLs
+// app.post('/api/shorturl', (req, res) => {
+//   const { url } = req.body;
+
+//   shortenUrl(url, (err, result) => {
+//     if (err) {
+//       return res.status(400).json({ error: 'invalid url' });
+//     }
+
+//     res.json(result);
+//   });
+// });
+
+// // Endpoint to redirect to the original URL
+// app.get('/api/shorturl/:shortKey', (req, res) => {
+//   const { shortKey } = req.params;
+//   const originalUrl = urlMap[parseInt(shortKey)];
+
+//   if (!originalUrl) {
+//     return res.status(404).json({ error: 'URL not found' });
+//   }
+
+//   res.redirect(originalUrl);
+// });
+
 // Endpoint to shorten URLs
 app.post('/api/shorturl', (req, res) => {
   const { url } = req.body;
@@ -68,6 +93,7 @@ app.get('/api/shorturl/:shortKey', (req, res) => {
 
   res.redirect(originalUrl);
 });
+
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
