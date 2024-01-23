9ff6f1a5154e52d84b8d441d447a8345a7a50d10
BENITA OJUGO EKENE-OKIKERE <dmnion4.glowd@gmail.com>
2024-01-23 13:49:52 +0100

    updateds

--- index.js
+++ index.js
@@ -83,16 +83,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((error) => {
-  console.error('Error connecting to MongoDB:', error);
-});
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  const db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
+  db.once('open', function() {
+    console.log("Connected to database.");
+  });
 
 // Define the Mongoose schema and model
 const Schema = mongoose.Schema;
